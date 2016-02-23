2016b9c06c0f3cca8ee67e4112d1f2cd790f3621
Evgeny Dudin <nagliyvred@gmail.com>
2016-02-23 20:59:18 +1100

    wrapped instrumentation as a gem

---
+++ lib/newrelic_patron/instrumentation.rb
@@ -0,0 +1,27 @@
+require 'patron'
+if defined?(::Patron) and not NewRelic::Control.instance['disable_patron'] and not ENV['NEWRELIC_ENABLE'].to_s =~ /false|off|no/i
+    ::Patron::Session.class_eval do
+      def request_with_newrelic_trace(action, url, headers, options = {})
+        newrelic_host = parse_host_for_newrelic url
+        metrics = ["External/#{newrelic_host}/Faraday::Connection/#{action}", "External/#{newrelic_host}/all", "External/all"]
+        if NewRelic::Agent::Transaction.recording_web_transaction?
+          metrics << "External/allWeb"
+        else
+          metrics << "External/allOther"
+        end
+        self.class.trace_execution_scoped metrics do
+          request_without_newrelic_trace(action, url, headers, options)
+        end
+      end
+      alias request_without_newrelic_trace request
+      alias request request_with_newrelic_trace
+
+      def parse_host_for_newrelic(url)
+        begin
+          URI.parse(base_url.empty? ? url : File.join(base_url, url)).host.to_s
+        rescue
+          ''
+        end
+      end
+    end
+end
---
+++ newrelic-patron.gemspec
@@ -0,0 +1,23 @@
+$LOAD_PATH.unshift 'lib'
+require "newrelic_patron/version"
+
+Gem::Specification.new do |s|
+  s.name = "newrelic_patron"
+  s.version = NewrelicPatron::VERSION::STRING
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.authors = ["Evgeny Dudin"]
+  s.description = "Patron instrumentation for Newrelic."
+  s.email = ["nagliyvred@gmail.com"]
+  s.has_rdoc = false
+  s.files = %w( README.md LICENSE newrelic-patron.gemspec )
+  s.files += Dir.glob("lib/**/*")
+  s.homepage = "http://github.com/nagliyvred/newrelic-patron"
+  s.require_paths = ["lib"]
+  s.rubygems_version = "1.5.3"
+  s.summary = "Patron instrumentation for Newrelic."
+  s.license = "MIT"
+
+  s.add_dependency(%q<patron>, ["~> 0"])
+  s.add_dependency(%q<newrelic_rpm>, ["~> 3.0"])
+end
